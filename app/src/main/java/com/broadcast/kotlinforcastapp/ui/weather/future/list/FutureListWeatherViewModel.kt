561c027b4d1bca661f3e1b7eb9ec1e75437402e9
hareshl <lakhwaniharesh134@gmail.com>
2019-09-20 19:04:42 +0530

    Initial commit

---
+++ app/src/main/java/com/broadcast/kotlinforcastapp/ui/weather/future/list/FutureListWeatherViewModel.kt
@@ -0,0 +1,7 @@
+package com.broadcast.kotlinforcastapp.ui.weather.future.list
+
+import androidx.lifecycle.ViewModel
+
+class FutureListWeatherViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
